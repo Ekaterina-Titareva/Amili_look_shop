e83c6e1ca05117d23f18dea052595eabb36c2a9f
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-06-09 22:15:38 +0300

    whatsapp

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -33,7 +33,7 @@ const Item = (props) => {
                     <p className={styles.color}>Цвет: {props.color}</p>
                     <p className={styles.material}>Материал: {props.material}</p>
                     <a className={styles.link} 
-                        href={`https://wa.me/89510686916?text=Здравствуйте, хочу сделать заказ. Меня интересует ${props.name}`} 
+                        href={"https://wa.me/79510686916?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5%2C%20%D1%85%D0%BE%D1%87%D1%83%20%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C%20%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7.%20%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20"} 
                         target="_blank">
                         заказать
                     </a>
